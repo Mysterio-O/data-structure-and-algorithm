1686d37d027c7d05b98b453d2a322f12f0570a0c
Mysterio-O <skrabbi.019@gmail.com>
2025-11-03 18:11:39 +0600

    hash table with reduce

--- time complexity/day 2/index.js
+++ time complexity/day 2/index.js
@@ -279,40 +279,87 @@ const total = cartItems.reduce((subtotal, car) => {
 
 // Scenario: Count every survey and group by response
 
-//? input
-const surveyResponses = [
-    "A",
-    "C",
-    "B",
-    "A",
-    "B",
-    "B",
-    "C",
-    "A",
-    "B",
-    "D",
-    "A",
-    "C",
-    "B",
-    "A",
+// //? input
+// const surveyResponses = [
+//     "A",
+//     "C",
+//     "B",
+//     "A",
+//     "B",
+//     "B",
+//     "C",
+//     "A",
+//     "B",
+//     "D",
+//     "A",
+//     "C",
+//     "B",
+//     "A",
+// ];
+
+// //? Output
+// // { A: 5, C: 3, B: 5, D: 1 }
+
+// const responseResult = surveyResponses.reduce((table, response) => {
+//     // ? this is more readable
+//     // if (table[response]) {
+//     //     table[response] = table[response] + 1;
+//     // } else {
+//     //     table[response] = 1;
+//     // };
+
+
+//     // ! this is short hand method. more clean, but less readable
+//     table[response] = (table[response] || 0) + 1;
+
+//     return table;
+// }, {});
+
+// console.log(responseResult);
+
+
+
+//* Grouping and Aggregating Data
+
+// Scenario: You have a flat array of sales data, and you need to group the sales by category,
+// calculating the total revenue and the number of items sold for each.
+
+const sales = [
+    { category: "Electronics", item: "Laptop", price: 1200, quantity: 1 },
+    { category: "Books", item: "JS Basics", price: 30, quantity: 2 },
+    { category: "Electronics", item: "Mouse", price: 25, quantity: 2 },
+    { category: "Home", item: "Chair", price: 150, quantity: 1 },
+    { category: "Books", item: "React Deep Dive", price: 50, quantity: 1 },
+    { category: "Electronics", item: "Keyboard", price: 80, quantity: 1 },
 ];
 
 //? Output
-// { A: 5, C: 3, B: 5, D: 1 }
+// {
+//   Electronics: {
+//     totalRevenue: 1330,
+//     itemCount: 4,
+//   },
+//   Books: {
+//     totalRevenue: 110,
+//     itemCount: 3,
+//   },
+//   Home: {
+//     totalRevenue: 150,
+//     itemCount: 1,
+//   },
+// };
 
-const responseResult = surveyResponses.reduce((table, response) => {
-    // ? this is more readable
-    // if (table[response]) {
-    //     table[response] = table[response] + 1;
-    // } else {
-    //     table[response] = 1;
-    // };
+const salesOutput = sales.reduce((table, sale) => {
+    const { category, price, quantity } = sale;
+    console.log(table,sale)
+    if (!table[category]) {
+        table[category] = {
+            totalRevenue: 0,
+            itemCount: 0
+        }
+    }
 
-
-    // ! this is short hand method. more clean, but less readable
-    table[response] = (table[response] || 0) + 1;
-
-    return table;
-}, {});
-
-console.log(responseResult);
+    table[category].totalRevenue += price * quantity;
+    table[category].itemCount += quantity
+    return table
+}, {})
