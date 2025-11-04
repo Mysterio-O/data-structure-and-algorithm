8bdab5e0511d6933d7aeb70672d828388a4b0f21
Mysterio-O <skrabbi.019@gmail.com>
2025-11-04 19:45:47 +0600

    added stack

---
+++ time complexity/day-3/data_structure_that_actually_matters/stack.js
@@ -0,0 +1,43 @@
+/**
+ * * there are two ways to implement stack
+ *  ? 1. Array
+ *  ? 2. Linked Lists 
+ */
+
+
+class Stack {
+    constructor() {
+        this.items = [];
+    };
+    isEmpty() {
+        return this.items.length === 0
+    };
+    length() {
+        return this.items.length;
+    }
+    push(item) {
+        this.items.push(item);
+    };
+    pop() {
+        if (this.isEmpty()) return undefined;
+        return this.items.pop()
+    };
+    peek() {
+        if (this.isEmpty()) return undefined;
+        return this.items[this.length() - 1];
+    };
+    print() {
+        console.log(this.items)
+    }
+}
+
+const stack = new Stack();
+
+console.log(stack.isEmpty());
+
+stack.push(2);
+stack.push(5)
+stack.push('string');
+console.log(stack.print());
+stack.pop();;
+console.log(stack.print());
