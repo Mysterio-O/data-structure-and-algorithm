35cdfe9ae4267a1f247c068278eabda19739679a
Mysterio-O <skrabbi.019@gmail.com>
2025-11-10 19:12:08 +0600

    added two pointer palindrome

---
+++ time complexity/day-4/palindrome.js
@@ -0,0 +1,26 @@
+const isPalindrome = (str) => {
+    const normalized = str.toLowerCase().replace(/[a-z0-9]/g, '');
+    const reversed = normalized.split('').reverse().join('')
+    if (normalized === reversed) return true
+    else return false
+};
+console.log(isPalindrome('doggoD'));
+console.log(isPalindrome('doggoDss'));
+
+
+const isPalindromeTwoPointer = (str) => {
+    const normalized = str.toLowerCase().replace(/[a-z0-9]/g, '');
+
+    let left = 0
+    let right = normalized.length - 1;
+
+    while (left < right) {
+        if (normalized[left] !== normalized[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+
+}
